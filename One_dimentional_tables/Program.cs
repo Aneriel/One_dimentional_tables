9757da9b0028aa228f221159e32e8c957990778e
bartlomiej.kaczmarsk <bartlomiej.kaczmarsk@PK6-06>
2022-11-04 10:46:34 +0100

    Dodaj pliki projektów.

---
+++ One_dimentional_tables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace wdp_lab_tablice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = Tab1D.GenTab() ; //size = 10, min = 1, max = 100
+            Console.WriteLine( Tab1D.ConvertToString(tab) );
+
+            int[] tab1 = Tab1D.GenTab(15, -5, 5);
+            Console.WriteLine( Tab1D.ConvertToString(tab1) );
+
+            (int min, int max) = Tab1D.MinMax(tab1); //trzeba zaprogramować MinMax
+            Console.WriteLine( $"min={min} max={max}" );
+        }
+    }
+}
